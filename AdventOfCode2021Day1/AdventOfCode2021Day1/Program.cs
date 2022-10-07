829f52874f93f670922975bdfd649e8cee736194
PVNagel <philip.v.nagel@gmail.com>
2022-10-07 11:03:28 +0200

    Added day 1-3

---
+++ AdventOfCode2021Day1/AdventOfCode2021Day1/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace AdventOfCode2021Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int j = 1;
+            int count = 0;
+
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\phili\Documents\Code\Exercises\AdventOfCode2021Day1\dataFile.txt");
+            int[] data = new int[lines.Length];
+
+            int localCount = 0;
+            int tempNumber;
+            foreach (string line in lines)
+            {
+                int.TryParse(line, out tempNumber);
+                data[localCount] = tempNumber;
+                localCount++;
+            }
+
+            for (int i = 0; i < data.Length - 1; i++)
+            {
+                if (data[j] > data[i])
+                {
+                    count++;
+                }
+                j++;
+            }
+
+            int result = 0;
+            for (int i = 3; i < data.Length; i++)
+            {
+                if (data[i] + data[i - 1] + data[i - 2] > data[i - 1] + data[i - 2] + data[i - 3])
+                {
+                    result++;
+                }
+            }
+            Console.WriteLine("The amount of times the submarine's depth increases: " + count);
+            Console.WriteLine("The version with less noise: " + result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AdventOfCode2021Day2/AdventOfCode2021Day2/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace AdventOfCode2021Day2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int horizontal = 0;
+            int depth = 0;
+            int x = 0;
+            int aim = 0;
+
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\phili\Documents\Code\Exercises\AdventOfCode2021Day2\dataFile.txt");
+
+            char seperator = ' ';
+
+            string[] command = new string[lines.Length];
+            int[] value = new int[lines.Length];
+
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string temp = lines[i];
+                string[] tempArray = temp.Split(seperator);
+                command[i] = tempArray[0];
+                value[i] = int.Parse(tempArray[1]);
+            }
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (command[i] == "forward")
+                {
+                    horizontal = horizontal + value[i];
+                    if (aim != 0)
+                    {
+                        depth = depth + (value[i] * aim);
+                    }
+                }
+
+                else if (command[i] == "up")
+                {
+                    aim = aim - value[i];
+                }
+
+                else if (command[i] == "down")
+                {
+                    aim = aim + value[i];
+                }
+            }
+            Console.WriteLine("horizontal position: " + horizontal + " Depth: " + depth);
+            Console.WriteLine("Position: " + (horizontal * depth));
+            Console.WriteLine("This Program includes part 2 of day 2.");
+            Console.ReadLine();
+        }
+    }
+}
